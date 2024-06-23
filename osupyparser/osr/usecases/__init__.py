1db522e4f4d09a95c0dc3674da2565c336d6e5f7
lenforiee <lenforiee@gmail.com>
2024-06-24 01:17:54 +0200

    general project structure

---
+++ osupyparser/osr/usecases/__init__.py
@@ -0,0 +1,4 @@
+from __future__ import annotations
+
+from . import read
+from . import write
---
+++ osupyparser/osr/usecases/read.py
@@ -0,0 +1,9 @@
+from __future__ import annotations
+
+from typing import BinaryIO
+
+
+def read_osr_file(file_path: str, pure_lzma: bool = False) -> ...: ...
+
+
+def read_osr_binary(file_binary: BinaryIO, pure_lzma: bool = False) -> ...: ...
