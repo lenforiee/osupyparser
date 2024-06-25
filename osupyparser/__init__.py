59d01de7305cadeb4ee4d3e320e4ba319bdec9fc
lenforiee <lenforiee@gmail.com>
2024-06-25 21:33:44 +0200

    `usecases` -> `parsers`

--- osupyparser/__init__.py
+++ osupyparser/__init__.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from osupyparser.common.constants.mode import Mode
 from osupyparser.common.constants.mods import Mods
 
-from osupyparser.osr.usecases.read import read_osr_file
-from osupyparser.osr.usecases.read import read_osr_file_lzma
-from osupyparser.osr.usecases.read import read_osr_binary
-from osupyparser.osr.usecases.read import read_osr_binary_lzma
+from osupyparser.osr.parsers.read import read_osr_file
+from osupyparser.osr.parsers.read import read_osr_file_lzma
+from osupyparser.osr.parsers.read import read_osr_binary
+from osupyparser.osr.parsers.read import read_osr_binary_lzma
